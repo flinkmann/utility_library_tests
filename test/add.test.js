66380e038ed156be15c24e79040cc9df4114a1bf
Test <email@address.com>
2024-12-08 17:15:23 +0200

    basic test

--- test/add.test.js
+++ test/add.test.js
@@ -1,11 +1,20 @@
-import chai from "chai"
-import add from "Utilities library\src\add.js"
+import { use, expect } from "chai"
+import add from "../src/add.js"
 
-const expect = chai.expect
+//const expect = chai.expect
 
 
 describe("Add utility function", () => {
  it("test addition 1", () =>{
-     expect(add(1,2)).to.equal(3)
+     expect(add(5,10)).to.equal(15)
  });
+ it("test addition 2", () =>{
+    expect(add(-1,2)).to.equal(1)
+});
+it("test addition 3", () =>{
+    expect(add(0,0)).to.equal(0)
+});
+it("test addition 4", () =>{
+    expect(add(-1,-1)).to.equal(-2)
+});
 })
